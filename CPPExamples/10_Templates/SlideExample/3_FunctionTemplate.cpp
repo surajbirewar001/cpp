f2b868ae979ad7c21f1ec5920bb1186eda7832bf
Suraj Birewar <132645617+surajbirewar001@users.noreply.github.com>
2023-09-23 11:51:39 +0530

    cpp

--- CPPExamples/10_Templates/SlideExample/3_FunctionTemplate.cpp
+++ CPPExamples/10_Templates/SlideExample/3_FunctionTemplate.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+template<class T>
+void swap(T &x, T &y)
+{
+    T temp = x;
+    x = y;
+    y = temp;
+}
+
+int main()
+{
+    int a = 10, b = 20;
+    swap(a, b);
+    cout << "\n" << a << " " << b;
+
+    float p = 10.10, k = 20.20;
+    swap(p, k);
+    cout << "\n" << p << " " << k;
+
+    return 0;
+}
